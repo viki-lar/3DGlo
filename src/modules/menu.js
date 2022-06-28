b8d33a04badc0be06344499d7784f6d3eb4c1e44
«viktoriya <viki.lar@yandex.ru>
2022-06-29 01:11:59 +0400

    делегирование событий для слушателей на кнопках меню

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -1,18 +1,33 @@
 const menu = () => {
   const menubtn = document.querySelector(".menu");
   const menu = document.querySelector("menu");
-  const closeBtn = menu.querySelector(".close-btn");
-  const menuItem = menu.querySelectorAll("ul>li>a");
+  const body = document.querySelector("body");
+  // const closeBtn = menu.querySelector(".close-btn");
+  // const menuItem = menu.querySelectorAll("ul>li>a");
 
   const handleMenu = () => {
     menu.classList.toggle("active-menu");
   };
 
-  menubtn.addEventListener("click", handleMenu);
-  closeBtn.addEventListener("click", handleMenu);
-  menuItem.forEach((item) => {
-    item.addEventListener("click", handleMenu);
+  // открытие меню
+  menubtn.addEventListener("click", (e) => {
+    if (e.target.closest(".menu")) {
+      handleMenu();
+    }
   });
+
+  //закрытие меню при клике на кнопку или на ссылку
+  menu.addEventListener("click", (e) => {
+    if (e.target.tagName === "A") {
+      handleMenu();
+    }
+  });
+
+  // menubtn.addEventListener("click", handleMenu);
+  // closeBtn.addEventListener("click", handleMenu);
+  // menuItem.forEach((item) => {
+  //   item.addEventListener("click", handleMenu);
+  // });
 };
 
 export default menu;
